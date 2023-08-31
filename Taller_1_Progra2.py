546b1d1eb4a91eb237df0c8126f4a0e93a981e81
EsneyderDuarteHernandez <142952613+EsneyderDuarteHernandez@users.noreply.github.com>
2023-08-30 20:54:34 -0500

    Add files via upload
    Se agrego función de validar numero al pedir las condiciones

--- Taller_1_Progra2.py
+++ Taller_1_Progra2.py
@@ -1,35 +1,50 @@
-def metodo_euler (x,y,paso,num_pasos,edo):
-    
-    
-    print("Solucion de Ecuacion diferencial ordinaria por el metodo de Euler")
+#Funcion para resolver ecuaciones ordinarias por el metodo de euler
+def metodo_euler (x,y,paso,num_pasos,edo):                      #Parametros que debe ingresar el usuario
+    print("Solucion de Ecuacion diferencial ordinaria por el metodo de Euler\n")
     print("Iteraciones\t x\t    y\n-------------------------------\n")
 
-    for i in range(int(x),num_pasos+1):
-           #x = cond_ini_x + paso
-            # y = cond_ini_y + paso
-            #y1 = y*eval(edo)
+    for i in range(int(num_pasos+1)):        
         print(f"\t{i}\t {x:.1f}\t {y:.4f}")     #Muestra la tabla
-        i += 1
-        ec = eval(edo)
-        x = x + paso
-            #y = y + paso 
-            #ec = eval(edo)
-
-        y = y + (paso * ec)
+        i += 1                                   # Indice
+        #ec = eval(edo)                           #Utilizamos la funcion eval para evaluar la exprecion en edo y le asigna el resultado de la expresion a ec
+        #print(ec)
+        y = y + paso * eval(edo)
+        x = x + paso                             #Formula de x
+                              # Formula de y
 
 # Función para validar variables en la ecuación diferencial
 def validar_xy(ecuacion):
-    variables_permitidas = 'xy'                             # Se enlistan las variables contenidas en la ecuación
+    ecuacion = ecuacion.lower()
+    variables_permitidas = "xy"                               # Se enlistan las variables contenidas en la ecuación
     
     for char in ecuacion:                                           # Se recorre la ecuacion para validación
         if char.isalpha() and (char not in variables_permitidas):
             return False
     return True
 
-x = float(input("Ingrese la condicion inicial de x(0):\t"))
-y = float(input("Ingrese la condicion inicial de y(0):\t"))
-h = float(input("Ingrese el tamaño del paso(h):\t"))
-num_pasos = int(input("Ingrese el numero de pasos (n):\t"))
+#Funcion para validar que ingresa un numero y retorna un float
+def validar_num(variable):                  
+    val1 = variable.replace('.','')         # Volvemos variable1 entero para utilizar la funcion isnumeric y evaluar si es numero
+    if (val1.isnumeric()):
+        variable = float(variable)              #Volvemos variable que es str en float
+        return (variable)
+    else:
+        while(True):           #mientras que variable sea diferente de un numero float y un numero entero
+            variable = input("La condicion debe ser un numero, ingrese un numero:\t")
+            val1 = variable.replace('.','')                         #en caso de que sea un float lo volvemos entero para evaluar si es numero
+            if (val1.isnumeric()):                                  #si es numero lo volvemos float si no es numero vuelve al  principio del while
+                variable = float(variable)
+                return (variable)
+                break
+
+x = input("Ingrese la condicion inicial de x(0):\t")
+x = validar_num(x)
+y = input("Ingrese la condicion inicial de y(0):\t")
+y = validar_num(y)
+h = input("Ingrese el tamaño del paso(h):\t")
+h = validar_num(h)
+num_pasos = input("Ingrese el numero de pasos (n):\t")
+num_pasos = validar_num(num_pasos)
 while (True):
     # Pedir la ecuación al usuario
     edo=input("Ingrese la ecuación diferencial ordinaria en terminos de 'x' y 'y':\t")
@@ -38,6 +53,6 @@ while (True):
         print("Ecuación válida")
         break
     else:
-        print("Ecuación inválida. Solo se permiten las letras x o y. \n")
+        print("Ecuación inválida. Solo se permiten las letras 'x' y 'y'. \n")
 metodo_euler(x,y,h,num_pasos,edo)
 
